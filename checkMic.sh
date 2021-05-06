609ad8f367cee80abd2a282fa98270fb9ff2429b
sozoro <46480375+sozoro@users.noreply.github.com>
2021-05-06 21:17:15 +0900

    add checkMic

---
+++ checkMic.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+jack_capture -p "system:capture_*" /dev/null
--- default.nix
+++ default.nix
@@ -1,16 +1,24 @@
-{ scriptPath ? ./capture_fullscreen_jackpulse.sh
-, scriptName ?
-    with lib; removeSuffix ".sh" (last (splitString "/" (toString scriptPath)))
+{ script1Path ? ./capture_fullscreen_jackpulse.sh
+, script1Name ?
+    with lib; removeSuffix ".sh" (last (splitString "/" (toString script1Path)))
+, script2Path ? ./checkMic.sh
+, script2Name ?
+    with lib; removeSuffix ".sh" (last (splitString "/" (toString script2Path)))
 , lib
 , runCommandLocal
 , makeWrapper
 , bash, procps, gnused, fzf, xrandr, gnugrep, gawk, ffmpeg, jack2
+, jack_capture
 }:
-runCommandLocal scriptName {
+runCommandLocal script1Name {
   nativeBuildInputs = [ makeWrapper ];
 } ''
-  makeWrapper ${scriptPath} $out/bin/${scriptName} \
+  makeWrapper ${script1Path} $out/bin/${script1Name} \
     --prefix PATH : ${lib.makeBinPath [
       bash procps gnused fzf xrandr gnugrep gawk ffmpeg jack2
+    ]}
+  makeWrapper ${script2Path} $out/bin/${script2Name} \
+    --prefix PATH : ${lib.makeBinPath [
+      bash jack_capture
     ]}
 ''
